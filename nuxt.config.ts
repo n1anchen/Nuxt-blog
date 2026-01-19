40113fb02317f201f455c2097bb895e03d04d274
Nianchen <n1anchen@outlook.com>
2026-01-19 17:55:47 +0800

    修改字体

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -25,10 +25,12 @@ export default defineNuxtConfig({
 				// "InterVariable", "Inter", "InterDisplay"
 				{ rel: 'stylesheet', href: 'https://rsms.me/inter/inter.css', media: 'print', onload: 'this.media="all"' },
 				// "JetBrains Mono", 思源黑体 "Noto Sans SC", 思源宋体 "Noto Serif SC"
-				{ rel: 'preconnect', href: 'https://fonts.gstatic.cn', crossorigin: '' },
-				{ rel: 'stylesheet', href: 'https://fonts.googleapis.cn/css2?family=JetBrains+Mono:ital,wght@0,100..800;1,100..800&family=Noto+Sans+SC:wght@100..900&family=Noto+Serif+SC:wght@200..900&display=swap', media: 'print', onload: 'this.media="all"' },
+				{ rel: 'preconnect', href: 'https://fonts.proxy.ustclug.org', crossorigin: '' },
+				{ rel: 'stylesheet', href: 'https://fonts.proxy.ustclug.org/css2?family=JetBrains+Mono:ital,wght@0,100..800;1,100..800&family=Noto+Serif+SC:wght@100..900&family=Noto+Serif+SC:wght@200..900&display=swap', media: 'print', onload: 'this.media="all"' },
 				// 小米字体 "MiSans"
-				{ rel: 'stylesheet', href: 'https://cdn-font.hyperos.mi.com/font/css?family=MiSans:100,200,300,400,450,500,600,650,700,900:Chinese_Simplify,Latin&display=swap', media: 'print', onload: 'this.media="all"' },
+				// { rel: 'stylesheet', href: 'https://cdn-font.hyperos.mi.com/font/css?family=MiSans:100,200,300,400,450,500,600,650,700,900:Chinese_Simplify,Latin&display=swap', media: 'print', onload: 'this.media="all"' },
+				// 霞鹜文楷
+				{ rel: 'stylesheet', href: 'https://cdnjs.webstatic.cn/ajax/libs/lxgw-wenkai-screen-webfont/1.7.0/style.min.css', media: 'print', onload: 'this.media="all"' },
 			],
 			templateParams: {
 				separator: '|',
